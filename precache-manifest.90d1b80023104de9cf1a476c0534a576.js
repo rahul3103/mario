21c8b396598240adb0d9738315260a62ad761c11
Rahul Shrivastava <rahul3103@gmail.com>
2018-12-23 14:01:31 +0530

    Updates

---
+++ precache-manifest.90d1b80023104de9cf1a476c0534a576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad9b5a98c54304ab20bec239cc3b578",
+    "url": "/mario/static/media/mushroom.0ad9b5a9.png"
+  },
+  {
+    "revision": "dbe70930c37478391700d657b45b0381",
+    "url": "/mario/static/media/mario.dbe70930.png"
+  },
+  {
+    "revision": "e4a9dc72f31e953842603790e016973d",
+    "url": "/mario/static/media/arrow.e4a9dc72.png"
+  },
+  {
+    "revision": "c1e026f1bba4a8f681e2",
+    "url": "/mario/static/js/runtime~main.c1e026f1.js"
+  },
+  {
+    "revision": "a2dd4c3f55642793530d",
+    "url": "/mario/static/js/main.a2dd4c3f.chunk.js"
+  },
+  {
+    "revision": "af2b06947b03c4cd2b94",
+    "url": "/mario/static/js/1.af2b0694.chunk.js"
+  },
+  {
+    "revision": "a2dd4c3f55642793530d",
+    "url": "/mario/static/css/main.b431bd53.chunk.css"
+  },
+  {
+    "revision": "8ec9bf729650f40d0dbc145bebe58d3d",
+    "url": "/mario/index.html"
+  }
+];
